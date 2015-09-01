fb2a3e1244e84849ed26e410a1e4a819a53fda33
tonycheng93 <bc1341286335@gmail.com>
2015-09-01 12:07:19 +0800

    添加是选中城市的判断

--- app/src/main/java/com/tony/iweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/tony/iweather/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.tony.iweather.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -55,6 +58,13 @@ public class ChooseAreaActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ChooseAreaActivity.this);
+        if (sp.getBoolean("city_selected",false)){
+            Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -71,6 +81,12 @@ public class ChooseAreaActivity extends Activity{
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(index);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String countyCode = countyList.get(index).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+                    intent.putExtra("county_code",countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
