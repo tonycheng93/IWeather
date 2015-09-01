ffc1f257db3476a8231261f7c29359795e68ac22
tonycheng93 <bc1341286335@gmail.com>
2015-09-01 14:52:23 +0800

    添加AutoUpdateService自动更新天气

---
+++ app/src/main/java/com/tony/iweather/service/AutoUpdateService.java
@@ -0,0 +1,63 @@
+package com.tony.iweather.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.tony.iweather.receiver.AutoUpdateReceiver;
+import com.tony.iweather.util.HttpCallbackListener;
+import com.tony.iweather.util.HttpUtil;
+import com.tony.iweather.util.Utility;
+
+public class AutoUpdateService extends Service {
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeather();
+            }
+        }).start();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8 * 60 * 60 * 1000;//8小时的毫秒数
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(AutoUpdateService.this,AutoUpdateReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(AutoUpdateService.this,0,i,0);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 更新天气
+     */
+    private void updateWeather() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this);
+        String weatherCode = sp.getString("weather_code", "");
+        String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                Utility.handleWeatherResponse(AutoUpdateService.this,response);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
