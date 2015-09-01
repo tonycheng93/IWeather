bfbae04e73783c6fa71e3c51cddd10ccef792cab
tonycheng93 <bc1341286335@gmail.com>
2015-09-01 10:59:11 +0800

    解析服务器返回的json数据并保存到sharedPreferences文件中

--- app/src/main/java/com/tony/iweather/util/Utility.java
+++ app/src/main/java/com/tony/iweather/util/Utility.java
@@ -1,11 +1,21 @@
 package com.tony.iweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 
 import com.tony.iweather.db.IWeatherDB;
 import com.tony.iweather.model.City;
 import com.tony.iweather.model.County;
 import com.tony.iweather.model.Province;
+
+import org.json.JSONObject;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * Created by Administrator on 2015/8/31.
@@ -73,5 +83,48 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 解析服务器返回的json数据，并将解析出来的数据存储到本地
+     */
+    public static void handleWeatherResponse(Context context,String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String pulishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,pulishTime);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中
+     * @param context
+     * @param cityName
+     * @param weatherCode
+     * @param temp1
+     * @param temp2
+     * @param weatherDesp
+     * @param pulishTime
+     */
+    private static void saveWeatherInfo(Context context, String cityName, String weatherCode,
+                                        String temp1, String temp2, String weatherDesp, String pulishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time",pulishTime);
+        editor.putString("current_data",sdf.format(new java.util.Date()));
+        editor.commit();
     }
 }
